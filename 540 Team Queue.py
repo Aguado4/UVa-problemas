0775df9f8651cb5c1924254d663bc195d6f8d397
Juan José Aguado <75216137+Aguado4@users.noreply.github.com>
2024-06-09 12:14:43 -0500

    Update 540 Team Queue.py

--- 540 Team Queue.py
+++ 540 Team Queue.py
@@ -1,6 +1,28 @@
 #Juan José Aguado
 #Código 8957833
 #Fecha 3-02-2023
+# Input
+"""
+The input file will contain one or more test cases. Each test case begins with the number of teams t (1 ≤ t ≤ 1 000). Then t
+team descriptions follow, each one consisting of the number of elements belonging to the team and the elements themselves.
+Elements are integers in the range 0..999 999. A team may consist of up to 1000 elements.
+Finally, a list of commands follows. There are three different kinds of commands:
+• ENQUEUE x — enter element x into the team queue
+• DEQUEUE — process the first element and remove it from the queue
+• STOP — end of test case
+The input will be terminated by a value of 0 for t.
+Warning: A test case may contain up to 200 000 (two hundred thousand) commands, so the implementation of the team
+queue should be efficient: both enqueing and dequeuing of an element should only take constant time.
+The input must be read from standard input.
+"""
+
+# Output
+"""
+For each test case, first print a line saying ‘Scenario #k’, where k is the number of the test case. Then, for each ‘DEQUEUE’
+command, print the element which is dequeued on a single line. Print a blank line after each test case, even after the last
+one.
+The output must be written to standard output.
+"""
 
 from collections import deque 
 

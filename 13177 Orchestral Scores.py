c6b20d77631256639d908e0e160e38a2fcaf48f1
Juan José Aguado <75216137+Aguado4@users.noreply.github.com>
2024-06-09 12:17:31 -0500

    Update 13177 Orchestral Scores.py

--- 13177 Orchestral Scores.py
+++ 13177 Orchestral Scores.py
@@ -6,6 +6,22 @@ from heapq import heappush, heapify, heappop
 from sys import stdin
 import math
 
+# Input
+"""
+Input will have several test cases, each of them composed by two lines. The first line contains two integers, p and n, with the
+number of scores we are allowed to buy (up to 200 000) and the number of different instruments that are part of the orchestra
+(up to 100 000). It is guaranteed that it will be possible to buy at least one stand for each type of instrument.
+The second line contains n positive numbers indicating the amount of musicians that are part of the orchestra for each of
+the instruments (up to 1 000).
+"""
+
+# Output
+"""
+For each test case, write a single integer indicating the number of musicians that will share the most crowded stand, always
+meeting the orchestra and budget restrictions. Don’t forget that the objective is to minimize the amount of people in the
+most crowded stand.
+"""
+
 def eje(partituras, lista):
     partituras -= len(lista)#las partituras "extra"
     cola = [(mus, mus, 1) for mus in lista] #creo una lista de tuplas con el numero de musicos y el tamaño del atril y el divisor/numero
